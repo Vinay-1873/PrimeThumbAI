3c881e16524de8fe43dc56cdcee7f99c59c986a4
vinay kumar <vinaycsc9@gmail.com>
2026-02-03 01:41:52 +0530

    feat: add optional.js and package.json for node-gyp-build
    feat: add HISTORY.md, LICENSE, README.md, and index.js for on-headers module
    
    feat: add package.json for on-headers module
    
    feat: add HISTORY.md, LICENSE, README.md, index.js, and package.json for random-bytes module
    
    feat: add HISTORY.md, LICENSE, README.md, index.js, and package.json for uid-safe module

---
+++ server/models/User.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name:{type:String, required:true ,trim:true},
+    email:{type:String,required:true,trim:true, unique:true,lowercase:true},
+}, {timestamps:true})
+
+const User = mongoose.model.User || mongoose.model('User',UserSchema)
+
+export default User
